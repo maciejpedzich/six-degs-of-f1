f158026ecd39424c04e63015e5d7c5094adcc1a6
Maciej Pędzich <contact@maciejpedzi.ch>
2024-04-27 23:54:35 +0200

    Create a global Neo4j connection instance

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,12 @@
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly NEO4J_DB_URI: string;
+  readonly NEO4J_USERNAME: string;
+  readonly NEO4J_PASSWORD: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
