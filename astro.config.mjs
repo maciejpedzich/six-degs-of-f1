77c401ac0e2885d92138454ea75b5cc3071dd9c3
Maciej Pędzich <contact@maciejpedzi.ch>
2024-04-29 22:14:53 +0200

    Use single quotes in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 
-import node from "@astrojs/node";
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: node({
-    mode: "standalone"
+    mode: 'standalone'
   })
 });
